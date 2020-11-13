f25297c82ce250748df73e1bb3ef77d900ed4da3
alefly <alyona.krebs@yandex.ru>
2020-11-14 01:25:38 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User petya = new User("Петя");
+            User dima = new User("Дима");
+            User gena = new User("Гена");
+            User masha = new User("Маша");
+            User sveta = new User("Света");
+
+            masha.follow(gena);
+            masha.follow(sveta);
+            dima.follow(petya);
+            sveta.follow(dima);
+            dima.follow(sveta);
+
+            masha.say("Привет!");
+            dima.say("Пока!");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class User
+    {
+        public string name { get; set; }
+        private List<User> followers { get; set; }
+
+        public User(string name) {
+            this.name = name;
+            followers = new List<User>();
+        }
+        public void follow(User user)
+        {
+            followers.Add(user);
+        }
+
+        public void unfollow(User user)
+        {
+            followers.Remove(user);
+        }
+
+        public void hear(string phrase, string nameSender)
+        {
+            Console.WriteLine($"Пользователь {name} услышал сообщение {phrase} от пользователя {nameSender}");
+        }
+
+        public void say(string phrase)
+        {
+            foreach (var user in followers)
+            {
+                user.hear(phrase, name);
+            }
+        }
+    }
+}
